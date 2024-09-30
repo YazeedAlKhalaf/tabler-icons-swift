092ca88899c2a8c48ed25223d3da103dcb5b7c53
Yazeed AlKhalaf <yazeed@alkhalaf.dev>
2024-09-30 16:25:29 +0300

    add initializer and macos support

--- Sources/TablerIcons/TablerIcons.swift
+++ Sources/TablerIcons/TablerIcons.swift
@@ -1,2 +1,24 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+public struct TablerIcons {
+
+}
+
+#if canImport(UIKit)
+import UIKit
+public extension UIImage {
+    convenience init?(tablerId: String) {
+        self.init(named: tablerId, in: Bundle.module, compatibleWith: nil)
+    }
+}
+#endif
+
+#if canImport(AppKit)
+import AppKit
+public extension NSImage {
+    static func image(tablerId: String) -> NSImage? {
+        Bundle.module.image(forResource: tablerId)
+    }
+}
+#endif
